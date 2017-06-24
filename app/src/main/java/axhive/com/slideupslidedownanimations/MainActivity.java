e7eb215bdecd8524cb8ec779d55eb8f4f4139259
Vlad Borovtsov <borovcov.vlad@gmail.com>
2017-06-24 12:53:59 +0700

    remove animations, cleanup

--- app/src/main/java/axhive/com/slideupslidedownanimations/MainActivity.java
+++ app/src/main/java/axhive/com/slideupslidedownanimations/MainActivity.java
@@ -9,33 +9,12 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Animation slideUpAnimation, slideDownAnimation;
-    ImageView imageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-
-        slideUpAnimation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.slide_up_animation);
-
-        slideDownAnimation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.slide_down_animation);
-        slideUpAnimation.setFillEnabled(true);
-        slideDownAnimation.setFillEnabled(true);
-
     }
 
-    public void startSlideUpAnimation(View view) {
-        imageView.clearAnimation();
-        imageView.startAnimation(slideUpAnimation);
-    }
 
-    public void startSlideDownAnimation(View view) {
-        imageView.clearAnimation();
-        imageView.startAnimation(slideDownAnimation);
-    }
 }
